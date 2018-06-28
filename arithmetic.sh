01d348ea250686cbb5e4ee108265bf2f87853afb
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-06-28 20:06:02 -0300

    Commit básico

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Basic arithmetic using let
+echo Basic arithmetic using let
 
 let a=5+4
 echo A = $a
@@ -18,8 +18,8 @@ let "c = $1 + 30"
 
 echo "C = $c"
 
-
-#Basic arithmetic using expr
+echo
+echo Basic arithmetic using expr
 
 expr 5 + 4
 
@@ -34,3 +34,38 @@ expr 11 % 2
 a=$( expr 10 - 3)
 
 echo $a
+
+echo
+echo Basic arithmetic using double parentheses
+
+a=$(( 4 + 5))
+echo $a
+
+a=$((3+5))
+echo $a
+
+b=$(( a + 3))
+echo $b
+
+b=$(( $a + 4))
+echo $b
+
+(( b++ ))
+echo $b
+
+(( b += 3))
+echo $b
+
+a=$(( 4 * 5))
+echo $a
+
+echo
+echo Show the length of a variable
+
+a='Hello World'
+echo ${#a}
+
+b=8547
+echo ${#b}
+
+
